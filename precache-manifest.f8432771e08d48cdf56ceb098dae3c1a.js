7debaa3d0d446fa97f2bbe53c1d00e77f6692b14
Jose L Garcia <70235472+jlgarciajr@users.noreply.github.com>
2024-03-11 02:55:41 -0500

    Updates

---
+++ precache-manifest.f8432771e08d48cdf56ceb098dae3c1a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6aa70b53e51e5325216d6205a9550005",
+    "url": "/cs378-p4/index.html"
+  },
+  {
+    "revision": "5f4fd4514574cdee7c1e",
+    "url": "/cs378-p4/static/css/main.e0fb0b68.chunk.css"
+  },
+  {
+    "revision": "0dc80ed45a74d03b7602",
+    "url": "/cs378-p4/static/js/2.91755b1d.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/cs378-p4/static/js/2.91755b1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b16f501c05a7a2c8b5cb",
+    "url": "/cs378-p4/static/js/3.15cc7b78.chunk.js"
+  },
+  {
+    "revision": "5f4fd4514574cdee7c1e",
+    "url": "/cs378-p4/static/js/main.a59a0df9.chunk.js"
+  },
+  {
+    "revision": "9ee2d9ecc8989b87153c",
+    "url": "/cs378-p4/static/js/runtime-main.dcd4e2d5.js"
+  }
+]);
